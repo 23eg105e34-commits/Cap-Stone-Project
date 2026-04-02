0b988b5e04a38d9e42b9c21930ddc738f008e222
Harsha Vardhan Reddy <23eg105e34@anurag.edu.in>
2026-04-02 19:50:28 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,10 @@ config(); //process.env
 //Create express application
 const app = exp();
 //use cors middleware
-app.use(cors({ origin: ["http://localhost:5173"],credentials:true }));
+app.use(cors({ origin: [
+  "http://localhost:5173",
+  "https://cap-stone-project-eight.vercel.app"
+],credentials:true }));
 //add body parser middleware
 app.use(exp.json());
 //add cookie parser middleware
